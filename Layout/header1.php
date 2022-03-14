2d8100a85801e7667234adce6d30b6bdd66d9f12
Soe Yadanar Tun <101356541+soeyadanartun@users.noreply.github.com>
2022-03-14 23:33:53 +0630

    Add files via upload

---
+++ Layout/header1.php
@@ -0,0 +1,49 @@
+<!-- navbar & banner -->
+<div class="gradient-color">
+    
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/images/logo.png" alt="" style="width: 50px; height: 50px;">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php#about">About us</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="index.php#courses" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Courses
+                        </a>
+                        <ul class="dropdown-menu gradient-color" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="primary.php#primary">Primary Education</a></li>
+                            <li><a class="dropdown-item" href="summer.php#summer">Summer Programs</a></li>
+                            <li><a class="dropdown-item" href="secondary.php#secondary">Secondary Education</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php#activities">Activities</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact us</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Sign In</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="register.php">Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    </div>
