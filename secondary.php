e0199abc61d8f1d88122cf00d69c6e3132318edc
ThaeNandarSeint <dardar2003nandar@gmail.com>
2022-03-17 22:43:50 +0630

    finish

--- secondary.php
+++ secondary.php
@@ -365,12 +365,10 @@
             },
             loop: true,
             breakpoints: {
-                // when window width is <= 499px
-                499: {
+                320: {
                     slidesPerView: 1,
                     spaceBetweenSlides: 50
                 },
-                // when window width is <= 999px
                 999: {
                     slidesPerView: 3,
                     spaceBetweenSlides: 50
